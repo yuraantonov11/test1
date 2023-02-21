ccc0f96a330c9a6eebd78217faa443689830176a
Yurii Antonov <yurii.antonov@globallogic.com>
2023-02-21 12:04:29 +0200

    Update

--- lib/board.dart
+++ lib/board.dart
@@ -1,71 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:test1/ship.dart';
 
 class Board extends StatefulWidget {
+  final List<List<int>> ships;
+  final List<List<int>> shots;
+  final Function(List<int>) onShot;
+
+  Board({required this.ships, required this.shots, required this.onShot});
+
   @override
   _BoardState createState() => _BoardState();
 }
 
 class _BoardState extends State<Board> {
-  List<List<String>> grid = List.generate(10, (_) => List.filled(10, ' '));
-  List<Ship> ships = <Ship>[];
-  int selectedShipIndex = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    generateShips();
-  }
-
-  void generateShips() {
-    ships.add(Ship('Aircraft Carrier', 5));
-    ships.add(Ship('Battleship', 4));
-    ships.add(Ship('Submarine', 3));
-    ships.add(Ship('Destroyer', 3));
-    ships.add(Ship('Patrol Boat', 2));
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.all(20),
-          child: const Text(
-            'Хід гравця',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Expanded(
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                // ...
-              });
-            },
-            child: Container(
-              margin: EdgeInsets.all(20),
-              child: GridView.builder(
-                itemCount: grid.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 10,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  int row = (index / 10).floor();
-                  int col = index % 10;
-                  String cellValue = grid[row][col];
-                  return Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey),
-                    ),
-                    child: Text(cellValue),
-                  );
-                },
-              ),
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 10,
+      ),
+      itemCount: 100,
+      itemBuilder: (context, index) {
+        final x = index % 10;
+        final y = index ~/ 10;
+        final hasShip = widget.ships.any((ship) => ship.contains(index));
+        final hasShot = widget.shots.contains([x, y]);
+
+        return GestureDetector(
+          onTap: () {
+            if (!hasShot) {
+              widget.onShot([x, y]);
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(),
+              color: hasShot ? Colors.blue : null,
+            ),
+            child: Center(
+              child: hasShip ? Icon(Icons.brightness_1) : null,
             ),
           ),
-        ),
-      ],
+        );
+      },
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,92 +1,34 @@
-import 'package:battleship_game/board.dart';
-import 'package:battleship_game/game_logic.dart';
-import 'package:battleship_game/ship.dart';
 import 'package:flutter/material.dart';
+import 'board.dart';
+import 'game.dart';
 
 void main() {
-  List<Ship> ships = [
-    Ship(name: 'Aircraft Carrier', length: 5),
-    Ship(name: 'Battleship', length: 4),
-    Ship(name: 'Submarine', length: 3),
-    Ship(name: 'Destroyer', length: 3),
-    Ship(name: 'Patrol Boat', length: 2),
-  ];
-
-  GameLogic game = GameLogic();
-  game.placeShips(ships);
-
-  runApp(MyApp(game));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final GameLogic game;
-
-  MyApp(this.game);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Battleship Game',
+      title: 'Морський бій',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Battleship Game', game: game),
+      home: GamePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title, this.game}) : super(key: key);
-  final String title;
-  final GameLogic game;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState(game);
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  GameLogic game;
-  Board playerBoard;
-  Board computerBoard;
-
-  _MyHomePageState(this.game);
-
-  @override
-  void initState() {
-    super.initState();
-    playerBoard = game.playerBoard;
-    computerBoard = game.computerBoard;
-  }
-
+class GamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Морський бій'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'Your Board',
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          BoardWidget(board: playerBoard, isPlayer: true),
-          SizedBox(height: 30),
-          Text(
-            'Computer Board',
-            style: Theme.of(context).textTheme.headline5,
-          ),
-          BoardWidget(board: computerBoard, isPlayer: false),
-        ],
-      ),
+      body: Game(),
     );
   }
 }
-
-class BoardWidget extends StatefulWidget {
-  final Board board;
-  final bool isPlayer;
-
-  BoardWidget({this.
