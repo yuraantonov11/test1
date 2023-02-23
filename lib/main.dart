64e1b4f3201302c041601e0a693d7d87130085d0
Yurii Antonov <yurii.antonov@globallogic.com>
2023-02-23 12:12:25 +0200

    add flutter_screenutil

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'app_localizations.dart';
 import 'screens/homepage/home_page.dart';
 import 'screens/settings/settings_screen.dart';
@@ -13,26 +14,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        AppLocalizations.delegate // доданий наш делегат
-      ],
-      supportedLocales: [
-        const Locale('en', ''),
-        const Locale('uk', '')
-      ],
-      title: 'Хрестики Нулики',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(),
-        '/settings': (context) => const SettingsScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(750, 1334),
+      builder: (BuildContext context, Widget? child) {
+        return Directionality(
+          textDirection: TextDirection.ltr,
+          child: Scaffold(
+            body: child,
+          ),
+        );
       },
+      child: MaterialApp(
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          AppLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en', ''),
+          Locale('uk', ''),
+        ],
+        title: 'Хрестики Нулики',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(),
+          '/settings': (context) => const SettingsScreen(),
+        },
+      ),
     );
   }
 }
